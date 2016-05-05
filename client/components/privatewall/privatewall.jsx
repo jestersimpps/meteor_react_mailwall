239b64192f2b9b87b66ed84a066d8036777c8624
jestersimpps <jestersimpps@gmail.com>
2016-05-05 21:11:09 +0200

    grid start

--- client/components/privatewall/privatewall.jsx
+++ client/components/privatewall/privatewall.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
 import {CardWrapper} from '../cardwrapper/cardwrapper.jsx';
+import {SpringGrid, layout, measureItems, makeResponsive} from 'react-stonecutter';
 
 // import {Posts} from '../../../collections/posts.js'
 
@@ -29,17 +30,20 @@ export class PrivateWall extends TrackerReact(React.Component) {
   render() {
 
     console.log(this.posts())
-
+    const Grid = makeResponsive(measureItems(SpringGrid), {
+      maxWidth: 1920,
+      minPadding: 10
+    });
     return (
-      <div>
+      <Grid component="span" className="grid" layout={layout.pinterest} columns={8} columnWidth={300} gutterWidth={15} gutterHeight={15} springConfig={{ stiffness: 170, damping: 26 }}>
         {this.posts().map((post) => {
           return (
-            <div className="brick white z-depth-1" key={post._id}>
+            <div className="brick white z-depth-1 hoverable" key={post._id}>
               <CardWrapper post={post}/>
             </div>
           )
         })}
-      </div>
+      </Grid>
     )
   }
 }
--- client/layouts/privatewall_layout.jsx
+++ client/layouts/privatewall_layout.jsx
@@ -4,8 +4,10 @@ import {NavBar} from '../components/navbar/navbar.jsx';
 export const PrivateWallLayout = ({content}) => (
   <div>
     <NavBar/>
-    <div className="navpage">
-      {content}
+    <div className="container-fluid ">
+      <div className="navpage">
+        {content}
+      </div>
     </div>
   </div>
 )
